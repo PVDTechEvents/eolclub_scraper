7b8470c92ff836896daf472adf504b604e895379
Matt Gillooly <matt@swipely.com>
2013-12-15 17:23:58 -0500

    split times on non-word character
    Ruby 1.9 was choking on the unicode dash I had here before.
    Thanks, Travis CI!

--- lib/eolclub_scraper/event_parser.rb
+++ lib/eolclub_scraper/event_parser.rb
@@ -11,7 +11,7 @@ module EolclubScraper
     def parse(content)
       doc = Nokogiri::HTML.parse(content)
       schedule_text = doc.css('p')[1].text.split("\n")[2].split(',').last.strip.split
-      start_time, end_time = schedule_text.last.split('–')
+      start_time, end_time = schedule_text.last.split(/\W/)
 
       Event.new(
         Chronic.parse( [ schedule_text[0], schedule_text[1], start_time ].join(' ') ),
